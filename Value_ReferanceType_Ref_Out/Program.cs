3d4f0c15cd409be60ca6fb2b8f80024ee408e376
seferovrusif1 <mi73sbd9a@code.edu.az>
2023-10-18 21:11:14 +0400

    Added task

---
+++ Value_ReferanceType_Ref_Out/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Value_ReferanceType_Ref_Out
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { -1, 2, -3, 4, -5, 6 };
+            int[] newarr;
+            Method(arr, out newarr);
+            foreach (var item in newarr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Method(int[] arr, out int[] newarr)   //Taski tam anlamadim anladigim qederi ile bunu istiyrsiz
+        {
+            for (int i = 0; i < arr.Length; i++)         
+            {
+                if (arr[i] < 0)
+                {
+                    arr[i] = (-1) * (arr[i]);
+                }
+            }
+            newarr = arr;
+
+        }
+
+    }
+}
